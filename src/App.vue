6783ae05ad0ebff0093ba5dcd2b2c4f308e2460a
Josh Ward <joshwardcodes@gmail.com>
2021-03-14 15:12:33 -0700

    start layout

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,53 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div
+    id="app"
+    class="flex flex-col min-h-screen text-gray-800"
+  >
+    <header
+      class="
+        flex-shrink-0
+        bg-gradient-to-r from-blue-400 to-blue-500
+        p-4 md:p-8
+        mb-4
+        shadow-md
+      "
+    >
+      <h1 class="text-3xl md:text-5xl font-bold">
+        Josh's Game Shelve
+      </h1>
+    </header>
+    <section class="flex-grow">
+      <router-view/>
+    </section>
+    <footer
+      class="
+        flex-shrink-0
+        bg-gray-200
+        p-4 md:p-8
+        mt-4
+      "
+    >
+      <p class="text-center font-semibold pb-2">
+        Site © 2021 Joshua Ward
+      </p>
+      <p class="text-gray-500 text-center">
+        Board game images and details provided by
+        <Link url="https://www.boardgamegeek.com/">BoardGameGeek</Link>
+        and is subject to their
+        <Link url="https://boardgamegeek.com/wiki/page/XML_API_Terms_of_Use">terms of use</Link>.
+      </p>
+    </footer>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import Link from '@/components/Link.vue'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+@Component({
+  components: {
+    Link
   }
-}
-</style>
+})
+export default class App extends Vue {}
+</script>
---
+++ src/components/Link.vue
@@ -0,0 +1,17 @@
+<template>
+  <a
+    :href="url"
+    class="text-blue-500 hover:text-blue-900"
+  >
+    <slot></slot>
+  </a>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component({})
+export default class Link extends Vue {
+  @Prop() url!: string
+}
+</script>
