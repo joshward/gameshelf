126416f583b86182a8740e4bdccf4b069f0f195d
Josh Ward <joshwardcodes@gmail.com>
2022-03-06 14:54:33 -0800

    add tracking

--- site/src/main.ts
+++ site/src/main.ts
@@ -4,6 +4,7 @@ import '@fontsource/cutive-mono'
 import '@fontsource/open-sans'
 import Vue from 'vue'
 import vueDebounce from 'vue-debounce'
+import { VuePlausible } from 'vue-plausible'
 import VTooltip from 'v-tooltip'
 import App from './App.vue'
 import router from './router'
@@ -13,6 +14,11 @@ Vue.config.productionTip = false
 
 Vue.use(vueDebounce, {
   fireOnEmpty: true
+})
+
+Vue.use(VuePlausible, {
+  domain: 'games.joshward.dev',
+  enableAutoOutboundTracking: true
 })
 
 Vue.use(VTooltip)
